cb39476c72e81b8a08d0d44f154193c666baadf3
Csaba Hruska <csaba.hruska@gmail.com>
2015-09-27 19:02:10 +0200

    Initial Commit

---
+++ Test4/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test4
+//
+//  Created by Csaba Hruska on 27/09/15.
+//  Copyright © 2015 Csaba Hruska. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test4/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test4
+//
+//  Created by Csaba Hruska on 27/09/15.
+//  Copyright © 2015 Csaba Hruska. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
